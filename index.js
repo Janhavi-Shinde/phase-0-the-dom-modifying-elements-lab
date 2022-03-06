009967f1a4a229863e1e7265586ff9abf56dac4f
janhavi <janashinde394@gmail.com>
2022-03-06 15:51:09 +1100

    all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+main.remove(); 
+
+// const main = document.getElementbyId('main')
+// main.remove();
+
+const newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML = 'Janhavi is the champion';
